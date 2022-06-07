0a899591522c5a2711ff61583f61c8a2a97d9f35
KanhaKanhaiya <thebig6project@gmail.com>
2022-06-07 12:32:39 +0000

    Improve AppStore

--- app/src/main/java/ml/test7777/big6/appstore/activities/MainActivity.kt
+++ app/src/main/java/ml/test7777/big6/appstore/activities/MainActivity.kt
@@ -3,8 +3,10 @@ package ml.test7777.big6.appstore.activities
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import ml.test7777.big6.appstore.adapters.AppListAdapter
 import ml.test7777.big6.appstore.custom.App
 import ml.test7777.big6.appstore.databinding.ActivityMainBinding
 
@@ -29,9 +31,12 @@ class MainActivity : AppCompatActivity() {
                 val appsList: List<App>
                 for (document in result) {
                     TODO("Add App List Handler")
+                }
 
+                val adapter = AppListAdapter(appsList)
+                binding.appsListRecyclerView.adapter = adapter
+                binding.appsListRecyclerView.layoutManager = LinearLayoutManager(this)
+                binding.appsListRecyclerView.setHasFixedSize(true)
             }
-
-        }
     }
 }
