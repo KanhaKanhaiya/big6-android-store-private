2d2e149a6e40db30effa760624be5e9ebff0e92e
KanhaKanhaiya <thebig6project@gmail.com>
2022-06-08 08:37:32 +0000

    Improve AppStore

--- app/src/main/java/ml/test7777/big6/appstore/activities/AppDetailsActivity.kt
+++ app/src/main/java/ml/test7777/big6/appstore/activities/AppDetailsActivity.kt
@@ -1,9 +1,11 @@
 package ml.test7777.big6.appstore.activities
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ml.test7777.big6.appstore.databinding.ActivityAppDetailsBinding
 
+@SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivityAppDetailsBinding
 
 class AppDetailsActivity : AppCompatActivity() {
