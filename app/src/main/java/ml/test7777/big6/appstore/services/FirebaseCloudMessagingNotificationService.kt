b9a919a9947e0c0181772d57804361d95de1169c
KanhaKanhaiya <thebig6project@gmail.com>
2022-06-14 16:11:07 +0000

    Add CloudMessaging

---
+++ app/src/main/java/ml/test7777/big6/appstore/services/FirebaseCloudMessagingNotificationService.kt
@@ -0,0 +1,12 @@
+package ml.test7777.big6.appstore.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FirebaseCloudMessagingNotificationService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
