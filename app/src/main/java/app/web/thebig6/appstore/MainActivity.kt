79c760378b9a4ab1d0fe4b017116bb8cd7699bab
KanhaKanhaiya <thebig6project@gmail.com>
2024-04-02 06:53:23 +0000

    Update dependencies

--- app/src/main/java/app/web/thebig6/appstore/MainActivity.kt
+++ app/src/main/java/app/web/thebig6/appstore/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +29,6 @@ class MainActivity : ComponentActivity() {
 fun ActivityLayout() {
     TheBig6ProjectAppStoreTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
         }
     }
 }
